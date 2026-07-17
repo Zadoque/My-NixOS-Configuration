3bea7640dec9fe145ff82d8f4ecc6f22729ad302
Zadoque Carneiro <139159544+Zadoque@users.noreply.github.com>
2026-07-17 12:17:43 -0300

    Add files via upload

---
+++ common.nix
@@ -0,0 +1,85 @@
+{ config, pkgs, ... }:
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.hplip ];
+  };
+
+  networking.networkmanager.enable = true;
+  networking.firewall.allowedTCPPorts = [ 6000 6001 ];
+
+  time.timeZone = "America/Sao_Paulo";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [
+    "pt_BR.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "es_ES.UTF-8/UTF-8"
+    "C.UTF-8/UTF-8"
+  ];
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "";
+  };
+  console.keyMap = "br-abnt2";
+
+  services.xserver.windowManager.i3.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  security.polkit.enable = true;
+  services.openssh.enable = true;
+
+  programs.zsh.enable = true;
+  environment.pathsToLink = [ "/share/zsh" ];
+
+  users.users.dock = {
+    isNormalUser = true;
+    description = "zadoque";
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  users.users.natalia = {
+    isNormalUser = true;
+    description = "Natalia";
+    shell = pkgs.bash;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      vlc
+      brave
+      chromium
+      libreoffice
+      zathura
+      kdePackages.okular
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  system.stateVersion = "26.05";
+}
--- flake.nix
+++ flake.nix
@@ -1,33 +1,43 @@
 {
-  description = "Meu sistema NixOS com Flakes e Home Manager";
+  description = "Configuração NixOS multi-host e multi-usuário com Home Manager e Zen Browser";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs:
+  let
+    mkHost = hostPath: nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
-        ./configuration.nix
+        hostPath
+        ./common.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.useGlobalPkgs    = true;
-          home-manager.useUserPackages  = true;
-          home-manager.users.dock       = import ./home.nix;
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.dock = import ./home/zadoque/home.nix;
+          home-manager.users.natalia = import ./home/esposa/home.nix;
         }
       ];
+    };
+  in {
+    nixosConfigurations = {
+      desktop = mkHost ./hosts/desktop/configuration.nix;
+      notebook = mkHost ./hosts/notebook/configuration.nix;
     };
   };
 }
